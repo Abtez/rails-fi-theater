30a636715cab8a46604843994b55eeb29e896c97
Abzed <ibraabzed@gmail.com>
2022-08-31 21:48:38 +0300

    add custom exceptional

--- app/models/production.rb
+++ app/models/production.rb
@@ -2,7 +2,18 @@ class Production < ApplicationRecord
     has_many :crew_members
 
     validates :title, presence: true
+    validates(:director, {:length => {:minimum => 5}})
+    validates :title, :uniqueness => true
+    validates :budget, length: {in: 400..1200}
     def title_director
         "#{title} : #{director}"
+    end
+
+    validate :no_more_anime
+
+    def no_more_anime
+        if genre.downcase == "anime"
+            errors.add(:genre, "No more anime")
+        end
     end
 end
