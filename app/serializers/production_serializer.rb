0abbe348d87b654d06f84b5ac45a7df3fcf70d88
Abzed <ibraabzed@gmail.com>
2022-09-01 18:08:07 +0300

    add serializers relationship

--- app/serializers/production_serializer.rb
+++ app/serializers/production_serializer.rb
@@ -1,5 +1,6 @@
 class ProductionSerializer < ActiveModel::Serializer
-  attributes :summary
+  attributes :summary, :title, :budget, :director, :image, :genre
+  has_many :crew_members
 
   def summary
     "The title is #{self.object.title} and is directed by #{self.object.director}"
